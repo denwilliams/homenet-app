6b3fdf180b7408ac31cd5679ec83e386ec44006e
Den Williams <dennis.williams@loke.com.au>
2016-06-03 21:50:36 +1000

    Zway

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,3 +1,4 @@
 export { HuePluginLoader } from 'homenet-plugin-hue';
 export { NinjaBlockPluginLoader } from 'homenet-plugin-ninjablock';
 export { MilightPluginLoader } from 'homenet-plugin-milight';
+export { ZwayPluginLoader } from 'homenet-plugin-zway';
