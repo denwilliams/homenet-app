389981e75fdb7981b40076c886d131edd736327f
Den Williams <dennis.williams@loke.com.au>
2016-07-19 21:32:45 +1000

    console stats

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,11 +4,30 @@ import core = require('homenet-core');
 import { HuePluginLoader, NinjaBlockPluginLoader, MilightPluginLoader/*, ZwayPluginLoader*/ } from '../plugins';
 import { join } from 'path';
 
-const configFilename = join(process.cwd(), process.argv[process.argv.length - 1]);
+
+@core.plugin()
+class ConsoleStats implements core.IStatsTarget {
+  constructor() {
+  }
+
+  gauge(id: string, value: number) : void {
+    console.log('guage:', id, value);
+  }
+
+  counter(id: string, increment?: number) : void {
+    console.log('counter:', id, increment);
+  }
+}
+
+(<any> core).registerStats(ConsoleStats);
+
+let configFilename = process.argv[process.argv.length - 1];
+if (configFilename[0] !== '/') configFilename = join(process.cwd(), configFilename);
 console.log('Loading with config ==> ' + configFilename);
 const config = require(configFilename);
 
 const runtime = core.init(RED, config);
+
 
 runtime.loadPlugin(HuePluginLoader);
 runtime.loadPlugin(MilightPluginLoader);
