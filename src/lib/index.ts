36583fb06f99659c3b56d558adaaf67c1971c0ab
Den Williams <dennis.williams@loke.com.au>
2016-09-27 18:47:40 +1000

    update packages and use zway plugin

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,7 +1,7 @@
 const RED = require('node-red');
 import core = require('homenet-core');
 
-import { HuePluginLoader, NinjaBlockPluginLoader, MilightPluginLoader/*, ZwayPluginLoader*/ } from '../plugins';
+import { HuePluginLoader, NinjaBlockPluginLoader, MilightPluginLoader, ZwayPluginLoader } from '../plugins';
 import { join } from 'path';
 
 
@@ -32,6 +32,6 @@ const runtime = core.init(RED, config);
 runtime.loadPlugin(HuePluginLoader);
 runtime.loadPlugin(MilightPluginLoader);
 runtime.loadPlugin(NinjaBlockPluginLoader);
-// runtime.loadPlugin(ZwayPluginLoader);
+runtime.loadPlugin(ZwayPluginLoader);
 
 runtime.start();
