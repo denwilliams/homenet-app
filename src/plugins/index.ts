1fe1753fc4b88dd42be7c3677e7ec30ce65da017
Den Williams <dennis.williams@loke.com.au>
2017-01-30 22:48:22 +1100

    update packages

--- src/plugins/index.ts
+++ src/plugins/index.ts
@@ -1,5 +1,5 @@
-export { HuePluginLoader } from 'homenet-plugin-hue';
-export { NinjaBlockPluginLoader } from 'homenet-plugin-ninjablock';
-export { MilightPluginLoader } from 'homenet-plugin-milight';
-export { FlicPluginLoader } from 'homenet-plugin-flic';
-export { ZwayPluginLoader } from 'homenet-plugin-zway';
+export { HuePluginLoader } from '@homenet/plugin-hue';
+export { NinjaBlockPluginLoader } from '@homenet/plugin-ninjablock';
+export { MilightPluginLoader } from '@homenet/plugin-milight';
+export { FlicPluginLoader } from '@homenet/plugin-flic';
+export { ZwayPluginLoader } from '@homenet/plugin-zway';
